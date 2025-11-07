44826c5865c2c541695c704dda221794a88e175d
Võ Ngọc Chánh <chanhkogaming@gmail.com>
2025-11-07 18:50:57 +0700

    BaiTap1

--- Lab1/hello.c
+++ Lab1/hello.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
-int main() {
-    int x,y;
-    int tong = 0;
-    printf("Hello, World!\n");
-    printf("Hello, World!\n");
-    printf("Nhap vao hai so nguyen x va y: ");
-    scanf("%d %d", &x, &y);
-    tong = x + y;
-    printf("Tong cua %d va %d la: %d\n", x, y, tong);
-    return 0;
+int main(){
+    int a,b;
+    printf("hello, chao cac ban");
+    printf("\nNhap vao a va b");
+    scanf("%d %d", &a,&b);
+return 0;
 }
