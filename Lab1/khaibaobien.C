dead6c00531629aa53c0d714564c02138a844ab1
Võ Ngọc Chánh <chanhkogaming@gmail.com>
2025-11-07 20:21:19 +0700

    Nop bai Lab 1

--- Lab1/khaibaobien.C
+++ Lab1/khaibaobien.C
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(){
-    int a,b;
+    int a,b;//khaibaobien
     printf("hello, chao cac ban");
     printf("\nNhap vao a va b");
-    scanf("%d %d", &a,&b);
+    scanf("%d %d", &a,&b);//nhaplieu
 return 0;
 }
